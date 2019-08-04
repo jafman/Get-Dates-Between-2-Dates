e5ab57ddeeccfd3ee7547d21fa9024699415507d
Jafar Kamarudeen <kamsy74@gmail.com>
2019-08-04 21:36:29 +0100

    Create date.js

---
+++ date.js
@@ -0,0 +1,9 @@
+var enumerateDaysBetweenDates = function(startDate, endDate) {
+    var now = startDate.clone(), dates = [];
+
+    while (now.isSameOrBefore(endDate)) {
+        dates.push(now.format('M/D/YYYY'));
+        now.add(1, 'days');
+    }
+    return dates;
+};
